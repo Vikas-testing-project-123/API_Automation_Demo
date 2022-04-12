f0d16c48a4b4f1ffc83d49a0ae1f90b988f7fd15
Vikas-testing-project-123 <99singh.vikash@gmail.com>
2022-04-12 17:26:06 +0530

    mail

--- sendReport.py
+++ sendReport.py
@@ -1,12 +1,37 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 
-server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-server.login("testtt211999@gmail.com", "Vikash@1999")#you need to change the security settings to login in
-server.sendmail("testtt211999@gmail.com",
-                "Vikas@kockpit.in",
-                "C:\Users\TA0134\PycharmProjects\API_Testing/Report.html")
+email_user = "testtt211999@gmail.com"
+email_send = "Vikas@kockpit.in"
+
+subject = "Response1"
+# recepent_list = ["Vikas@kockpit.in", "99singh.vikash@gmail.com"]
+
+msg = MIMEMultipart()
+msg['From'] = email_user
+msg['To'] = email_send
+msg['Subject'] = subject
+
+body = "Hi, here is teh report of the API test"
+msg.attach(MIMEText(body, 'plain'))
+
+filename = 'report.html'
+attachment = open(filename,'rb')
+
+part = MIMEBase('application', 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('content-Disposition',"attachment; filename="+filename)
+
+msg.attach(part)
+text = msg.as_string()
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(email_user, "Vikash@1999")#you need to change the security settings to login in
+server.sendmail(email_user, email_send, text)
 
 server.quit()
 print("Done")
