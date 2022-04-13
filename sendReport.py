2f7cf260688fa2a339dc6cebfb0b1fc948ed503e
Vikas-testing-project-123 <99singh.vikash@gmail.com>
2022-04-13 11:24:30 +0530

    mailsend

--- sendReport.py
+++ sendReport.py
@@ -5,20 +5,20 @@ from email.mime.base import MIMEBase
 from email import encoders
 
 email_user = "testtt211999@gmail.com"
-email_send = "Vikas@kockpit.in"
+# email_send = "Vikas@kockpit.in", "99singh.vikash@gmail.com"
 
 subject = "Response1"
 # recepent_list = ["Vikas@kockpit.in", "99singh.vikash@gmail.com"]
 
 msg = MIMEMultipart()
 msg['From'] = email_user
-msg['To'] = email_send
+# msg['To'] = email_send
 msg['Subject'] = subject
 
-body = "Hi, here is teh report of the API test"
+body = "Hi, here is the report of the API test"
 msg.attach(MIMEText(body, 'plain'))
 
-filename = 'report.html'
+filename = "report.html"
 attachment = open(filename,'rb')
 
 part = MIMEBase('application', 'octet-stream')
@@ -28,10 +28,12 @@ part.add_header('content-Disposition',"attachment; filename="+filename)
 
 msg.attach(part)
 text = msg.as_string()
+
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
 server.login(email_user, "Vikash@1999")#you need to change the security settings to login in
-server.sendmail(email_user, email_send, text)
+server.sendmail(email_user, ["Vikas@kockpit.in","snehal.katare@kockpit.in", "abhishek.p@kockpit.in"
+                             ,"meenakshi.b@kockpit.in","vibhor@kockpit.in"], text )
 
 server.quit()
 print("Done")
